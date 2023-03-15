0e9c295a37eb05f7455899bdb23577444278cf11
chaaanhee <cksgml8157@gmail.com>
2023-03-15 15:48:22 +0900

    Initial commit

---
+++ src/main/java/com/shop/Billione/BillioneApplication.java
@@ -0,0 +1,13 @@
+package com.shop.Billione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/Billione/BillioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.Billione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
